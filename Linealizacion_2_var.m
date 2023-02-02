a57d66104d5eb11000486f85e1d0c57542bed057
Johann <johann200_cardenas788@hotmail.com>
2023-02-02 16:59:57 -0600

    teminando clases 3

--- Linealizacion_2_var.m
+++ Linealizacion_2_var.m
@@ -7,3 +7,8 @@ lf1 = taylor(f1,[x s], [x0 sl0], "order", 2)
 
 f2 =  ((mumax*x*s)/((ks+s)*yxs));
 lf2 = taylor(f2, [x s], [x0 s10], "order", 2)
+
+F = [f1; f2];
+jF = jacobian(F,[x s]);
+
+jF2 = subs(jF, [x s], [x0 sl0]);
